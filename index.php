2a6eb1003d5c34a1a50ea4e78e593eacb4ab3070
Nouman Rahman <techcode786@gmail.com>
2021-11-16 21:31:14 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="index.css">
+	<link rel="stylesheet" href="extras/prism.css">
+	<script defer src="extras/prism.js"></script>
+	<title>PHP Tutorial</title>
+</head>
+
+<body>
+	<div class="container">
+		<div class="header">
+			<h1>Learn PHP The Right Way!</h1>
+			<hr>
+			<h2>Section 1 - Basics</h2>
+			<pre><code class="language-php">echo "Hello world"</code></pre>
+		</div>
+	</div>
+</body>
+
+</html>
